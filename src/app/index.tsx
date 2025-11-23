0506cdea98f7e775ec76d3c8d0da110242384627
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-11-22 22:23:35 -0600

    hook useMenu

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,36 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { api } from "../services/api";
-import { getMenu } from "../services/menu.service";
-import { MenuItem } from "../interfaces/menu.interface";
-import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Image,
+} from "react-native";
+import { useMenu } from "../hooks/useMenu";
 
 export default function App() {
-  const [menu, setMenu] = useState<MenuItem[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { menu, loading } = useMenu();
 
-  useEffect(() => {
-    loadMenu();
-  }, []);
-
-  const loadMenu = async() => {
-    setLoading(true);
-    try {
-      const data = await getMenu();
-      setMenu(data);
-      console.log(data)
-    } catch (error) {
-      console.error("Error loading menu:", error);
-    } finally {
-      setLoading(false);
-    }
-  }
+  if (loading)
+    return (
+      <ActivityIndicator
+        size="large"
+        color="#0000ff"
+        style={styles.loadinContainer}
+      />
+    );
 
   return (
-    <View style={styles.container}>
-      <Text>Menú Screen</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={menu}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <View style={{ marginVertical: 10, padding: 10, borderWidth: 1 }}>
+          <Image
+            source={{ uri: item.image }}
+            style={{ width: 100, height: 100, marginBottom: 10 }}
+          />
+          <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+          <Text>Price: ${item.price}</Text>
+          <Text>Category: {item.category}</Text>
+        </View>
+      )}
+    />
   );
 }
 
@@ -38,6 +43,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+  },
+
+  loadinContainer: {
+    flex: 1,
     justifyContent: "center",
   },
 });
---
+++ src/hooks/useMenu.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { MenuItem } from "../interfaces/menu.interface";
+import { getMenu } from "../services/menu.service";
+
+export const useMenu = () => {
+  const [menu, setMenu] = useState<MenuItem[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadMenu();
+  }, []);
+
+  const loadMenu = async () => {
+    setLoading(true);
+    try {
+      const data = await getMenu();
+      setMenu(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error loading menu:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { menu, loading };
+};
